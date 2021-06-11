1778527b7701490676c711e0db7a7db474c3e3e1
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2021-06-10 21:19:03 -0400

    ejemplos de variables

---
+++ clase 9_20210610/main.js
@@ -0,0 +1,36 @@
+
+// var
+// let
+// const
+
+var miEdad = 28
+console.info(miEdad)
+console.log(typeof(miEdad))
+
+miEdad = "28 anios"
+console.info(miEdad)
+console.log(typeof(miEdad))
+
+var miedad = 300;
+console.info(miedad)
+console.log(typeof(miedad))
+
+
+const PI = 3.14
+let mivariable = 30
+
+let esMayorDeEdad = true;
+console.info(esMayorDeEdad)
+console.log(typeof(esMayorDeEdad))
+
+esMayorDeEdad = false;
+console.info(esMayorDeEdad)
+console.log(typeof(esMayorDeEdad))
+
+esMayorDeEdad = null;
+console.info(esMayorDeEdad)
+console.log(typeof(esMayorDeEdad))
+
+
+
+
